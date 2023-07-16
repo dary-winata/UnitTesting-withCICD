f28913c64f2a3c9db00fe799d870d467715121a6
dary-winata <dary.winata15@gmail.com>
2023-07-16 11:48:51 +0700

    Initial Commit

---
+++ LearningUnitTest/LearningUnitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningUnitTestApp.swift
+//  LearningUnitTest
+//
+//  Created by dary winata nugraha djati on 16/07/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningUnitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
